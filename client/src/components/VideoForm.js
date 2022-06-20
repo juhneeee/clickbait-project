553d1c6df32d72c8fa70e0aad36e7d66324455c8
Johnny Chen <johnnyc1314@gmail.com>
2022-06-20 16:28:32 -0400

    added text to button

--- client/src/components/VideoForm.js
+++ client/src/components/VideoForm.js
@@ -1,15 +1,26 @@
-import react from "react";
-
+import React, {useState} from "react";
+const API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&key=AIzaSyBRtr7ks8BNgFQd06r36SwIp58Iy2bimSY&id="
 
 function VideoForm() {
+    const [urlInput, setUrlInput] = useState("")
 
 
-    return <form>
-        <input type="text" name="url" placeholder="url"></input>
+    function handleSubmit(e){
+        e.preventDefault()
+        const id = urlInput
+        console.log({id: id})
+        // fetch(API + id)
+        
+    }
+    
+
+    return <form onSumbit={handleSubmit}>
+        <input type="text" name="url" placeholder="url" value={urlInput} onChange={(e)=>setUrlInput(e.target.value)}></input>
         <input type="text" name="title" placeholder="title"></input>
         <input type="text" name="thumbnail" placeholder="thumbnail"></input>
-        <button type="submit"></button>
+        <button type="submit">Submit</button>
     </form>
 }
 
 export default VideoForm;
+
