5c3e9b02d1115f27c8422556395124e25cf0f5b9
Johnny Chen <johnnyc1314@gmail.com>
2022-06-20 20:28:50 -0400

    added routes to videos. can look up youtube api

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,2 +1,20 @@
 class VideosController < ApplicationController
+
+    def index
+        videos = Video.all
+        render json: videos
+    end
+    def show
+        video = video.find(:id)
+        render json: video
+    end
+    def create
+        video = Video.create!(video_params)
+        render json: video, status: 201
+    end
+
+    private
+    def video_params
+        params.permit(:title, :thumbnail, :url, :uploader_id )
+    end
 end
--- client/src/components/VideoForm.js
+++ client/src/components/VideoForm.js
@@ -1,20 +1,41 @@
 import React, {useState} from "react";
-const API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&key=AIzaSyBRtr7ks8BNgFQd06r36SwIp58Iy2bimSY&id="
+const youtubeAPI = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&key=AIzaSyBRtr7ks8BNgFQd06r36SwIp58Iy2bimSY&id="
+const API = "https://vast-wave-75628.herokuapp.com/"
 
 function VideoForm() {
     const [urlInput, setUrlInput] = useState("")
 
-
     function handleSubmit(e){
         e.preventDefault()
-        const id = urlInput
+        const id = urlInput.match(/=[a-z0-9]+/i)[0].slice(1)
         console.log({id: id})
-        // fetch(API + id)
-        
+        fetch(youtubeAPI + id)
+        .then(r => r.json())
+        .then(data => {
+            const snippet = data.items[0].snippet
+            const videoObj = {
+                title: snippet.title,
+                thumbnail: snippet.thumbnails.standard.url,
+                url: id,
+                uploader_id: null
+            }
+            addVideo(videoObj)
+        })
+    }
+
+    function addVideo(obj){
+        fetch(API + "video", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(obj)
+        })
+        //then do something i guess
     }
     
 
-    return <form onSumbit={handleSubmit}>
+    return <form onSubmit={handleSubmit}>
         <input type="text" name="url" placeholder="url" value={urlInput} onChange={(e)=>setUrlInput(e.target.value)}></input>
         <input type="text" name="title" placeholder="title"></input>
         <input type="text" name="thumbnail" placeholder="thumbnail"></input>
